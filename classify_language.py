65d09e22531b78331d648987c4bd571e68d76cfb
CongNguyen <cong.nguyen@sotatek.com>
2019-10-29 17:16:32 +0700

    translate comment into vietnamese

--- classify_language.py
+++ classify_language.py
@@ -1,20 +1,29 @@
+# - *- coding: utf- 8 - *-
 import numpy as np
 from readData import DataSource
-from textblob import TextBlob
+# from textblob import TextBlob
 from langdetect import detect
+from googletrans import Translator
 
 
 def classify_lang(label):
     ds = DataSource()
     tot = ds.loadData(label)
+    translator = Translator()
     raw_data = np.char.strip(tot, chars=label)
     array_vi = []
     for text in raw_data:
-        lang = detect(text)
-        if lang != "vi":
+        dete = translator.detect(text)
+        print text
+        # lang = detect(text)
+        if dete.lang == "vi" and dete.confidence == 1:
             array_vi.append(text)
+        else:
+            # - *- coding: utf- 8 - *-
+            newText = translator.translate(text, dest='vi').text
+            newnew = newText.encode("utf8")
+            array_vi.append(newnew)
     np_array_vi = np.array(array_vi)
-    print(np_array_vi.size)
     return np_array_vi
 
 
@@ -22,7 +31,7 @@ def create_vi_file(name_file, label):
     f = open(name_file, "w+")
     np_temp_array = classify_lang(label)
     for line in np_temp_array:
-        f.write(label+line)
+        f.write((label+ " " +line + "\n"))
     f.close()
 
 
